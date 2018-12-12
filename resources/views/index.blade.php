e87bc76d73f59052dde66cf04610dcaa66391563
Lucas Duarte da Costa <lucas.duarte@academico.ifrn.edu.br>
2018-12-12 08:50:52 -0300

    Colocanco projeto no Laravel

---
+++ resources/views/index.blade.php
@@ -0,0 +1,118 @@
+@extends('template.base')
+
+@section('adicao')
+<div class="jumbotron centro col-12">
+	<h1 class="display-4">Bem Vindo ao Tech & CIA</h1>
+	<p class="lead">Este site tem uma grande variedade de peça para celulares.</p>
+	<hr class="my-4">
+	<p>Além de outros serviços.</p>
+	<a class="btn btn-primary btn-lg" href="#" role="button">Veja mais</a>
+</div>
+
+@endsection
+
+
+@section('conteudo')
+<div class="area col-12">
+	<div class="row">
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto1.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>			
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto2.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto3.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto1.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>			
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto2.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto3.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto1.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>			
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto2.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto3.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto1.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>			
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto2.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>
+		<div class="card col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
+			<img class="card-img-top t" src="assets/img/produto3.jpg" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title">Produto X</h5>
+				<p class="card-text">Estiloso, confortável e com tecnologia avançada.</p>
+				<a href="#" class="btn btn-primary">Mais informações</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+@endsection
